7c8ff58c010125e490c883e8bf2e02cb7cf4274a
WalukaUI <cwaluka@yahoo.com>
2021-08-29 22:29:46 -0700

    completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,24 @@
 class AuthorsController < ApplicationController
+ rescue_from ActiveRecord::RecordInvalid, with: :error_message
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.create!(author_params)
+      render json: author, status: :created
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def error_message(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
   
 end
